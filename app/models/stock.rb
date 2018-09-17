b9fdc962590bb43f6c70763818e09a48f962daf6
takacchi <lkr.rafa@gmail.com>
2018-09-17 16:12:29 +0000

    Add user stock assosiaction and views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
   
   def self.new_from_lookup(symbol)
     begin
@@ -9,4 +11,7 @@ class Stock < ApplicationRecord
     end
   end
   
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
 end
